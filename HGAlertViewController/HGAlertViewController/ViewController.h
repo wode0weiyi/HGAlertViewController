a5ca65800092c74554db5f5b60f124e70bce117c
huzhihui <wonderfulhzh@163.com>
2018-08-31 10:12:38 +0800

    222
    222

---
+++ HGAlertViewController/HGAlertViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HGAlertViewController
+//
+//  Created by 胡志辉 on 2018/8/31.
+//  Copyright © 2018年 胡志辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
